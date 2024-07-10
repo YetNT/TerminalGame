572c72b8c954b9e26f7496db6acf59390e2aea8d
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-07-10 19:45:23 +0200

    test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,5 @@
 #include "../include/TerminalGame.hpp"
+#include "../include/TerminalColor.hpp"
 #include <iostream>
 #include <string>
 
@@ -6,25 +7,15 @@ int main() {
     TerminalGame game;
     game.clearConsole();
 
-    // std::pair<short, short> a = game.getTerminalSize();
+    Sprite p({ "AB", "CD" }, { { TerminalColor::RED, TerminalColor::CYAN}, {TerminalColor::MAGENTA, TerminalColor::YELLOW} });
 
-    bool ld = false;
-    struct coords {
-        int x;
-        int y;
-    };
-    coords place = { 0, 0 };
-    for (int r = 0;r <= 40; r++) {
-        if (ld) {
-            place.x++;
-            game.renderer.drawChar('O', place.x, place.y);
-        }
-        else {
-            place.y++;
-            game.renderer.drawChar('O', place.x, place.y);
-        }
-        if (r % 10 == 0) ld = !ld;
-        game.wait(100);
+    game.renderer.addSprite(p);
+
+    int x = 1;
+    while (x != 0) {
+        game.renderer.renderFrame();
+        std::cin >> x;
+        p.move(x, 0);
     }
 
     std::string e;
